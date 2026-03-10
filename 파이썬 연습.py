f0018df592bb33689f86c02c6eed98ae26fbfcac
simhyelin <hyelisim9@gmail.com>
2026-03-10 15:57:09 +0900

    Add files via upload

---
+++ 파이썬 연습.py
@@ -0,0 +1,26 @@
+a=print('hello python!')
+print(a)
+
+def plus(a,b):
+    c = a+b
+    return c
+ans = plus(4,3)
+print(ans)
+
+print('Hello World!')
+print('Hello', 'World!')
+print('Hello'+'World!')
+print(100)
+print(2+4)
+
+#circle_with_var.py
+radius = 4.0
+print('원의 반지름', radius)
+print('원의 면적', 3.14 * radius * radius)
+print('원의 둘레', 2.0 * 3.14 * radius)
+
+x=100
+y=200
+
+z=108
+w=24
