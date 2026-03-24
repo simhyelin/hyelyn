754518a2ea6dd2d83068a7226014fc343a5d3d09
simhyelin <hyelisim9@gmail.com>
2026-03-24 21:04:08 +0900

    Add files via upload

--- LAB 3-2.py
+++ LAB 3-2.py
@@ -11,9 +11,9 @@ if n % 2 == 0:
 x = int(input("정수를 입력하세요: "))
 print("x =", x)
 if x >= 0:
-    print("x 은(는) 자연수입니다.")
+    print(x, "은(는) 자연수입니다.")
 
 x = int(input("정수를 입력하세요: "))
 print("x =", x)
 if x >= 0:
-    print("x 은(는) 자연수입니다.")
+    print(x, "은(는) 자연수입니다.")
